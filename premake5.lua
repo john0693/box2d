4c260eecf52750486f7d0e8797adbc9b5042671f
john0693 <john0693@gmail.com>
2021-04-07 00:19:42 -0500

    added premake file

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "box2d"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/box2d/*.h",
+		"src/collision/*.cpp",
+		"src/common/*.cpp",
+		"src/dynamics/*.h",
+		"src/rope/*.cpp"
+	}
+	
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
